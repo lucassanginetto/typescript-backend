e4545a2f21aca2a3b04a6633d3ae51db41284522
Lucas Sanginetto <lucassanginetto@protonmail.com>
2025-05-09 00:54:49 -0300

    use cors

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import './connection'
+import './connection';
 import productController from './controllers/product.controller';
+import cors from 'cors';
 
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/api/products', productController.create);
 app.get('/api/products', productController.findAll);
