e4bffeadf7922064b849819ff2e90f7388463684
Lucas Sanginetto <lucassanginetto@protonmail.com>
2025-05-13 16:28:34 -0300

    make DataSource get its options from dotenv

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # TypeScript Back-end
 
 NodeJS API using TypeORM, PostgreSQL and Docker.
-Based on Especializati's "TypeScript on the Back-end" course ([repo](https://github.com/especializati/typescript-back-end-typeorm)). 
+
+Based on Especializati's "TypeScript on the Back-end" course ([repo](https://github.com/especializati/typescript-back-end-typeorm)).
 
 ## Running
 
 ```sh
+# Run PostgreSQL Docker container
+docker-compose up -d
+
 # Install Node dependencies
 yarn install
 
-# Run PostgreSQL Docker container
-docker-compose up -d
+# Create dotenv file using the example
+cp .env.example .env
 
 # Run API
 yarn run dev
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,13 +1,28 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm";
 
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined;
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_NAME;
+
+const ca = process.env.DB_SSL_CA;
+let ssl = undefined;
+if (ca) {
+  ssl = {
+    rejectUnauthorized: true,
+    ca
+  };
+}
+
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5433,
-  username: "admin",
-  password: "admin",
-  database: "ts_node",
+  host,
+  port,
+  username,
+  password,
+  database,
   synchronize: true,
   logging: true,
   entities: [
@@ -15,7 +30,8 @@ const AppDataSource = new DataSource({
   ],
   subscribers: [],
   migrations: [],
-})
+  ssl
+});
 
 AppDataSource.initialize()
   .then(() => console.log('Connected successfully'))
