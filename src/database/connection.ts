785f17bcbe7c1c9212bccc3b7de7d905e004f3ab
Lucas Sanginetto <lucassanginetto@protonmail.com>
2025-07-08 23:34:40 -0300

    import entity directly instead of using glob pattern

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,4 +1,5 @@
 import { DataSource } from "typeorm";
+import { Product } from "../entities/product.entity";
 
 const AppDataSource = new DataSource({
   type: "postgres",
@@ -10,7 +11,7 @@ const AppDataSource = new DataSource({
   synchronize: true,
   logging: true,
   entities: [
-    __dirname + '/../**/*.entity.ts'
+    Product
   ],
   subscribers: [],
   migrations: [],
